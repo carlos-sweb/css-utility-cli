7ebca00c4788e8a6a1540292272bb64c2dab2d3c
carlos-sweb <c4rl0sill3sc4@gmail.com>
2021-12-22 13:40:25 -0300

    update

---
+++ README.md
@@ -0,0 +1,35 @@
+# css-utili CLI
+
+Un programa que genera recursos css para tu projecto.
+
+# ¿ Como comenzar ?
+
+## Requisitos previos
+
+crystal-lang instalado
+
+## Paso 1
+
+Descarga este repositorio
+
+console´
+
+git clone https://github.com/carlos-sweb/css-utility-cli.git
+
+´
+
+## Paso 2
+
+Compila el archivo cr con el siguiente comando
+
+console´
+crystal build css-utility --release
+´
+
+## Paso 3
+
+descubre como usar css-utility
+
+´
+css-utility --help
+´
---
+++ css-utility.cr
@@ -0,0 +1,29 @@
+require "colorize"
+require "option_parser"
+
+#https://crystal-lang.org/reference/1.2/getting_started/cli.html
+
+OptionParser.parse do |parser|
+  parser.banner = "Welcome to Css-Utility"
+
+  parser.on "-v", "--version", "Show version" do
+    puts "version 1.0"
+    exit
+  end
+  parser.on "-h", "--help", "Show help" do
+    puts parser
+    exit
+  end
+  parser.on "-c PROJECT", "--create=PROJECT", "create css-utility project" do | project |
+    puts " Project : #{project.colorize(:green)} created"
+    puts " The Next step is run this command"
+    puts "  cd #{project} && css-utility init"
+    exit
+  end
+  parser.on "-i", "--init", "initialize css-utility project" do
+    puts "Project : aaaa created"
+    puts "cd aaa && run css-utility init"
+    exit
+  end
+
+end
