be8bfed920c26edd614e2c691da11bd434207390
carlos-sweb <c4rl0sill3sc4@gmail.com>
2021-12-25 02:53:41 -0300

    update

--- css-utility-cli.cr
+++ css-utility-cli.cr
@@ -2,8 +2,7 @@ require "colorize"
 require "file_utils"
 require "option_parser"
 require "yaml"
-
-#https://crystal-lang.org/reference/1.2/getting_started/cli.html
+# https://crystal-lang.org/reference/1.2/getting_started/cli.html
 propertys = [
   "alignment",
   "backgrounds",
@@ -17,28 +16,25 @@ propertys = [
   "sizing",
   "spacing",
   "tables",
-  "typography"
+  "typography",
 ]
-
 config_property = [] of String
-config_property.insert(0, {{ `cat #{__DIR__}/config/property/alignment.yaml`.stringify }} )
-config_property.insert(1, {{ `cat #{__DIR__}/config/property/backgrounds.yaml`.stringify }} )
-config_property.insert(2, {{ `cat #{__DIR__}/config/property/bgcolor.yaml`.stringify }} )
-config_property.insert(3, {{ `cat #{__DIR__}/config/property/border.yaml`.stringify }} )
-config_property.insert(4, {{ `cat #{__DIR__}/config/property/effects.yaml`.stringify }} )
-config_property.insert(5, {{ `cat #{__DIR__}/config/property/flexbox.yaml`.stringify }} )
-config_property.insert(6, {{ `cat #{__DIR__}/config/property/grid.yaml`.stringify }} )
-config_property.insert(7, {{ `cat #{__DIR__}/config/property/interactivity.yaml`.stringify }} )
-config_property.insert(8, {{ `cat #{__DIR__}/config/property/layout.yaml`.stringify }} )
-config_property.insert(9, {{ `cat #{__DIR__}/config/property/layout.yaml`.stringify }} )
-config_property.insert(10, {{ `cat #{__DIR__}/config/property/sizing.yaml`.stringify }} )
-config_property.insert(11, {{ `cat #{__DIR__}/config/property/spacing.yaml`.stringify }} )
-config_property.insert(12, {{ `cat #{__DIR__}/config/property/tables.yaml`.stringify }} )
-config_property.insert(13, {{ `cat #{__DIR__}/config/property/typography.yaml`.stringify }} )
-
+config_property.insert(0, {{ `cat #{__DIR__}/config/property/alignment.yaml`.stringify }})
+config_property.insert(1, {{ `cat #{__DIR__}/config/property/backgrounds.yaml`.stringify }})
+config_property.insert(2, {{ `cat #{__DIR__}/config/property/bgcolor.yaml`.stringify }})
+config_property.insert(3, {{ `cat #{__DIR__}/config/property/border.yaml`.stringify }})
+config_property.insert(4, {{ `cat #{__DIR__}/config/property/effects.yaml`.stringify }})
+config_property.insert(5, {{ `cat #{__DIR__}/config/property/flexbox.yaml`.stringify }})
+config_property.insert(6, {{ `cat #{__DIR__}/config/property/grid.yaml`.stringify }})
+config_property.insert(7, {{ `cat #{__DIR__}/config/property/interactivity.yaml`.stringify }})
+config_property.insert(8, {{ `cat #{__DIR__}/config/property/layout.yaml`.stringify }})
+config_property.insert(9, {{ `cat #{__DIR__}/config/property/layout.yaml`.stringify }})
+config_property.insert(10, {{ `cat #{__DIR__}/config/property/sizing.yaml`.stringify }})
+config_property.insert(11, {{ `cat #{__DIR__}/config/property/spacing.yaml`.stringify }})
+config_property.insert(12, {{ `cat #{__DIR__}/config/property/tables.yaml`.stringify }})
+config_property.insert(13, {{ `cat #{__DIR__}/config/property/typography.yaml`.stringify }})
 OptionParser.parse do |parser|
   parser.banner = "Welcome to Css-Utility"
-
   parser.on "-v", "--version", "Show version" do
     puts "version 1.0"
     exit
@@ -47,89 +43,112 @@ OptionParser.parse do |parser|
     puts parser
     exit
   end
-  parser.on "-c PROJECT", "--create=PROJECT", "create css-utility project" do | project |
+  parser.on "-c PROJECT", "--create=PROJECT", "create css-utility project" do |project|
     # create project and create config file
-      if Dir.exists?(project)
+    if Dir.exists?(project)
       puts " The directory #{project.colorize(:green)} is already created"
     else
-      Dir.mkdir( project  )
-      Dir.mkdir( project+"/config" )
-      Dir.mkdir( project+"/config/property" )
-      Dir.mkdir( project+"/dist" )
-      Dir.mkdir( project+"/sass" )
-
+      Dir.mkdir(project)
+      Dir.mkdir(project + "/config")
+      Dir.mkdir(project + "/config/property")
+      Dir.mkdir(project + "/dist")
+      Dir.mkdir(project + "/scss")
+      Dir.mkdir(project + "/scss/property")
       config_state = {{ `cat #{__DIR__}/config/state.yaml`.stringify }}
       config_screen = {{ `cat #{__DIR__}/config/screen.yaml`.stringify }}
-
-      File.write(project+"/config/screen.yaml", config_screen )
-      File.write(project+"/config/state.yaml", config_state )
-
+      File.write(project + "/config/screen.yaml", config_screen)
+      File.write(project + "/config/state.yaml", config_state)
       iterator = 0
       while iterator <= config_property.size
-        File.write(project+"/config/property/#{propertys[iterator]}.yaml", config_property[iterator] )
+        File.write(project + "/config/property/#{propertys[iterator]}.yaml", config_property[iterator])
         iterator += 1
-        break if iterator >= (config_property.size-1)
+        break if iterator >= (config_property.size - 1)
       end
-
       puts " Project : #{project.colorize(:green)} created !"
       puts " The next step is run this command"
       puts "  cd #{project.colorize(:green)} && css-utility init"
-
     end
     exit
   end
   parser.on "-i", "--init", "initialize css-utility project" do
-    puts "Project : aaaa created"
-    puts "cd aaa && run css-utility init"
+    puts ":("
     exit
   end
   parser.on "-b", "--build", "build project" do
-      if Dir.exists?("config")
-        # ======================================================================
-        screen = File.open("config/screen.yaml") do | file |
-          YAML.parse(file).as_h
-        end
-        #  procesamos el archivo screen
-        # lo conertimos en un archivo sass
-        # se declaran como variables generales
-        screenContent = ""
-        screen.each do | key , value |
-            screenContent += "@#{key}("
-            iterator_screen = 0
-            value.as_h.each do | k , v |
-                if iterator_screen == (value.as_h.size-1)
-                    screenContent += "\"#{k}\":#{v}"
-                else
-                    screenContent += "\"#{k}\":#{v},"
-                end
-                iterator_screen += 1
-            end
-            screenContent +=")\n"
-        end
-        File.write("sass/screen.sass",screenContent)
-        # ======================================================================
-        # ======================================================================
-        state = File.open("config/state.yaml") do | file |
-          YAML.parse(file).as_a
-        end
-        #  procesamos el archivo screen
-        # lo conertimos en un archivo sass
-        # se declaran como variables generales
-        stateContent = "@state:"
-        stateIterator = 0
-        state.each do |  value |
-          stateContent +=" #{value}"
-          if stateIterator < (state.size-1)
-            stateContent +=","
-          end
-          stateIterator += 1
-        end
-        File.write("sass/state.sass",stateContent)
-        # ======================================================================
-      else
-          puts "Error:".colorize(:red)
-          puts " Dont exists config folder"
+    if Dir.exists?("config")
+      # ======================================================================
+      screen = File.open("config/screen.yaml") do |file|
+        YAML.parse(file).as_h
       end
+      #  procesamos el archivo screen
+      # lo conertimos en un archivo scss
+      # se declaran como variables generales
+      screenContent = ""
+      screen.each do |key, value|
+        screenContent += "$#{key}("
+        iterator_screen = 0
+        value.as_h.each do |k, v|
+          if iterator_screen == (value.as_h.size - 1)
+            screenContent += "\"#{k}\":#{v}"
+          else
+            screenContent += "\"#{k}\":#{v},"
+          end
+          iterator_screen += 1
+        end
+        screenContent += ");\n"
+      end
+      File.write("scss/screen.scss", screenContent)
+      # ======================================================================
+      # ======================================================================
+      state = File.open("config/state.yaml") do |file|
+        YAML.parse(file).as_a
+      end
+      #  procesamos el archivo screen
+      # lo conertimos en un archivo scss
+      # se declaran como variables generales
+      stateContent = "$state:"
+      stateIterator = 0
+      state.each do |value|
+        stateContent += " #{value}"
+        if stateIterator < (state.size - 1)
+          stateContent += ","
+        else
+          stateContent += ";"
+        end
+        stateIterator += 1
+      end
+      File.write("scss/state.scss", stateContent)
+      # ======================================================================
+      propertys.each do |_property|
+        baseUrlProperty = "config/property/"
+        if File.exists?(baseUrlProperty + _property + ".yaml")
+          propertyYaml = File.open(baseUrlProperty + _property + ".yaml") do |file|
+            YAML.parse(file)
+          end
+          propertyContent = ""
+          propertyYaml.as_h.each do |key, value|
+            value.as_h.each do |k, v|
+              propertyContent += ".#{k}{\n"
+              propertyContent += "  #{key}:#{v};\n"
+              propertyContent += "}\n"
+            end
+          end
+          propertyContent += "@each $value in $state{\n"
+          propertyYaml.as_h.each do |key, value|
+            value.as_h.each do |k, v|
+              propertyContent += " .#{k}\\:\#{$value}:\#{$value}{\n"
+              propertyContent += "   #{key}:#{v};\n"
+              propertyContent += " }\n"
+            end
+          end
+          propertyContent += "}\n"
+          File.write("scss/property/" + _property + ".scss", propertyContent)
+        end
+      end
+    else
+      puts "Error:".colorize(:red)
+      puts " Dont exists config folder"
+    end
     exit
   end
   parser.invalid_option do |flag|
@@ -138,5 +157,4 @@ OptionParser.parse do |parser|
     STDERR.puts parser
     exit(1)
   end
-
 end
