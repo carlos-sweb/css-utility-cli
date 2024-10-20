3782667a92ea0a709e3b0a46806347f2aee12a13
carlos-sweb <c4rl0sill3sc4@gmail.com>
2024-10-20 03:16:59 -0300

    add crow server to main file

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,24 @@
 #include "create_project.h"
 #include "build_project.h"
 
+#define CROW_STATIC_DIRECTORY "/home/sweb/cppProjects/css-utility-cli/public/"
+//#define CROW_STATIC_ENDPOINT "/alternative_endpoint/<path>"
+//#define CROW_DISABLE_STATIC_DIR
+
+
+#include "crow.h"
+
+
+#include <random>
+
+
+int numeroAleatorio(int min, int max) {
+    static std::mt19937 generador(std::time(0));
+    std::uniform_int_distribution<int> distribucion(min, max);
+    return distribucion(generador);
+}
+
+
 // -----------------------------------------------------------------------------------------------
 int main(int argc, char* argv[]){
     // -------------------------------------------------------------------------------------
@@ -28,7 +46,7 @@ int main(int argc, char* argv[]){
         if(cmdl[{"-h","--help"}]){ ho.help(); }
     }    
     if( cmdl[{"-i","--init"}] ){
-	    std::cout << "init work\n";
+        std::cout << "Try Init Working...\n";
     }
     // -------------------------------------------------------------------------------------
     // Aqui sabemos que esta declaro con un = , auque de todas 
@@ -42,6 +60,45 @@ int main(int argc, char* argv[]){
     // Aqui sabemos que viene --build="somethink" de todas manera podria venir vacia 
     if(cmdl({"-b","--build"})){buildProject(cmdl({ "-b", "--build" }).str());}    
     // -------------------------------------------------------------------------------------
+    if(cmdl[{"-s","--server"}]){
+
+        // int port = numeroAleatorio(8000,125000);
+
+        crow::SimpleApp app;        
+        crow::mustache::set_base("/home/sweb/cppProjects/css-utility-cli/public");
+        crow::mustache::set_global_base("/home/sweb/cppProjects/css-utility-cli/public");
+
+        CROW_ROUTE(app, "/")([](){
+            std::string name = "Carlos";                                        
+            auto page = crow::mustache::load("index.html"); // 
+            crow::mustache::context ctx ({{"person", name}}); // 
+            return page.render(ctx); //
+        });
+        
+         CROW_ROUTE(app, "/bulma.css")
+    ([](const crow::request&, crow::response& res) {
+        //replace cat.jpg with your file path
+        res.set_static_file_info("css/bulma.min.css");
+        res.end();
+    });
+
+        CROW_ROUTE(app, "/json")
+        ([]{
+
+            crow::json::wvalue x({{"message", "Hello, World!"}});
+            x["message2"] = "Hello, World.. Again!";
+            return x;
+        });
+        CROW_ROUTE(app,"/hello/<string>")
+        ([](std::string s){
+            return crow::response(s);
+        });
+        // app.port(port).multithreaded().run();  
+        app.port(18080).multithreaded().run();  
+
+    }    
+
+
     return EXIT_SUCCESS;
     // -------------------------------------------------------------------------------------
 }
