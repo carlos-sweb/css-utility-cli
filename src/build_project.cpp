a06864933bdc5efc10b3f16842e3d64ebf076685
carlos-sweb <c4rl0sill3sc4@gmail.com>
2024-12-05 01:36:10 -0300

    save

--- src/build_project.cpp
+++ src/build_project.cpp
@@ -37,15 +37,26 @@ void buildProject(std::string path){
  
     std::ofstream archivo( MaterMinCss , std::ios::out | std::ios::trunc);
     // NORMALIZE SECTION
+
+
     bool normalize = d.HasMember("normalize") ?  d["normalize"].IsBool() ? d["normalize"].GetBool()  : false  : false;
-    if( d.HasMember("normalize") ){
+    if( d.HasMember("normalize") ){        
         if( !d["normalize"].IsBool() ){
             ho.warningMessage("The normalize option must be of boolean type");            
         }
     }
     if( normalize ){
+        fs::path NormalizeFilePath = pathBuild / "dist" / "normalize.min.css";
+        if( !fs::exists( NormalizeFilePath ) ){
+            std::ofstream NormalizeFile( NormalizeFilePath , std::ios::out | std::ios::trunc );
+            if( NormalizeFile.is_open() ){
+                NormalizeFile << normalize_min_css;
+                NormalizeFile.close();
+            }               
+        }        
         if (archivo.is_open()){
-            archivo << normalize_min_css;                                
+            archivo << "@import url(\"normalize.min.css\");";
+            /*normalize_min_css;*/
         }            
     }
     global_build_default Build_default; 
@@ -58,30 +69,36 @@ void buildProject(std::string path){
     const Value &states = d["states"];
     const Value &categories = d["categories"];
     const Value &screens = d["screens"];
-    /* Bloque de Prueba ---------------------------------- */
-    Build_default.eachCategories( categories , [&path,&archivo,&states,&Build_default](global_css_category *categoryBuild){
-        // Aqui debemos crear los archivos por categorias
-        // std::cout << categoryBuild->name_category  << "\n";        
-    });
-    /* Bloque de Prueba ---------------------------------- */
+    
 
     // Compilación Principal del archivo master.css
     Build_default.eachScreens(screens,[&archivo,&states,&Build_default,&categories](std::string name , std::string min , std::string max){        
-        archivo << std::string(fmt::format(" @media screen and (min-width:{0}) and (max-width:{1}){{",min,max));        
+        // archivo << std::string(fmt::format(" @media screen and (min-width:{0}) and (max-width:{1}){{",min,max));        
         Build_default.eachCategories( categories , [&archivo,&states,&Build_default,&name](global_css_category *categoryBuild){
-            archivo << categoryBuild->cssScreenWrapper(name);
+            // archivo << categoryBuild->cssScreenWrapper(name);
             Build_default.eachStates( states ,[&archivo,&categoryBuild,&name](const char* state){
-                archivo << categoryBuild->cssScreenWrapper(name,state);
+                // archivo << categoryBuild->cssScreenWrapper(name,state);
             });
         });
-        archivo << "}";        
+        // archivo << "}";        
     });
     // Compilación Principal del archivo master.css
     
-    Build_default.eachCategories( categories , [&archivo,&states,&Build_default](global_css_category *categoryBuild){
-        archivo << categoryBuild->css();
+    Build_default.eachCategories( categories , [&archivo,&states,&Build_default,&pathBuild](global_css_category *categoryBuild){
+        std::cout << "Compile ---------------------------------------------\n";
+        fs::path pathCategoryCssFile = pathBuild / "dist" /"category" / fmt::format("{0}.min.css",categoryBuild->name_category);
+        std::ofstream categoryCssFile(pathCategoryCssFile, std::ios::out | std::ios::trunc );
+        if( categoryCssFile.is_open() ){
+            categoryCssFile << categoryBuild->css();
+            categoryCssFile.close();
+        }
+        std::cout << fmt::format("category/{0}.min.css",categoryBuild->name_category)  << "\n";            
+        // archivo << categoryBuild->css();
+        archivo << fmt::format("@import url(\"category/{0}.min.css\");",categoryBuild->name_category);
+        std::cout << "------------------------------------------------------\n";
         Build_default.eachStates( states ,[&archivo,&categoryBuild](const char* state){
-            archivo << categoryBuild->css(state);
+            // archivo << categoryBuild->css(state);
+            archivo << fmt::format("@import url(\"category/{0}/{1}.min.css\");",state,categoryBuild->name_category);
         });
     });
      
